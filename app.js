171d6e76a09af9253e0cb68c4c9bdedcfda84241
aksungurlu <t.aksungurlu@gmail.com>
2018-10-01 00:21:12 +0300

    Add isLoggedIn

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/secret", (req, res) => {
+app.get("/secret", isLoggedIn, (req, res) => {
   res.render("secret");
 });
 
@@ -70,6 +70,13 @@ app.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, () => {
   console.log("Server is started...");
