4b5ebe9602f2020a0686ad3c5741ac7fea6a4dc4
aksungurlu <t.aksungurlu@gmail.com>
2018-10-01 00:16:45 +0300

    Add register, login and logout

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-var express   = require("express"),
-    app       = express(),
-    mongoose  = require("mongoose");
+var express           = require("express"),
+    app               = express(),
+    mongoose          = require("mongoose"),
+    bodyParser        = require("body-parser"),
+    passport          = require("passport"),
+    LocalStrategy     = require("passport-local"),
+    passportMongoose  = require("passport-local-mongoose"),
+    User              = require("./models/user"),
+    sessionApp        = require("express-session");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 mongoose.connect("mongodb://localhost/auth-demo");
+
+app.use(sessionApp({
+  secret: "Kaan is the best",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//ROUTES
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -11,6 +34,41 @@ app.get("/", (req, res) => {
 
 app.get("/secret", (req, res) => {
   res.render("secret");
+});
+
+//AUTH ROUTES
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  User.register(new User({username: req.body.username}), req.body.password, (err, user) => {
+    if(err){
+      console.log(err);
+      return res.render("register");
+    } else{
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secret");
+      });
+    }
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secret",
+  failureRedirect: "/login"
+}) ,(req, res) => {
+
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
